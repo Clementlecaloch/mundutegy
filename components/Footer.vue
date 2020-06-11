3fd61243fc4ebb6a15d855e2879202f58745c2ef
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-06-11 16:35:49 +0200

    css form

--- components/Footer.vue
+++ components/Footer.vue
@@ -20,20 +20,22 @@
           <img class="footer-img" src="../assets/image/eux5.jpg" alt="">
         </div>
       </div>
-        <p class="text-center"><a href="mailto:natgil.mundutegy@gmail.com">Nous contacter sur : natgil.mundutegy@gmail.com</a></p>
-        <form name="ask-question"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field">
-          <input type="hidden" name="form-name" value="ask-question" />
-          <label>Your Name: <input type="text" name="name" /></label>
-          <label>Your Email: <input type="email" name="email" /></label>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-          <label>Message: <textarea name="message"></textarea></label>
-          <button type="submit">Send</button>
+      <h3 class="text-center padding-20">Une question ? contactez nous</h3>
+        <form name="ask-question" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <div class="form">
+            <input type="hidden" name="form-name" value="ask-question" />
+            <label for="nom">Nom :</label>
+            <input type="text" name="name" id="nom" />
+            <label for="mail">Mail :</label>
+            <input type="email" name="mail" id="mail">
+            <label for="subject">Sujet :</label>
+            <input type="text" name="subject" id="subject">
+            <label for="message">Message: </label>
+            <textarea name="message" id="message"></textarea>
+          </div>
+          <div class="container-button text-center">
+            <button type="submit" class="button button-form">Envoyer</button>
+          </div>
         </form>
         <p class="text-center"><a href="https://clementlecaloch.now.sh">Par Clément Le Caloch</a></p>
     </footer>
@@ -50,6 +52,10 @@
     background-color: black;
   }
 
+  footer h3 {
+    color: white;
+  }
+
 .footer-img {
   width: 100%;
   height: 100%;
@@ -61,5 +67,10 @@
   p {
     padding: 20px;
     color:white
+  }
+
+  .button-form {
+    filter: invert();
+    background-color: white;
   }
 </style>
