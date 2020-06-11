8a69cf62a52ee30fc2e0f0565138653291626201
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-06-11 16:10:30 +0200

    test form 2

--- components/Footer.vue
+++ components/Footer.vue
@@ -21,25 +21,19 @@
         </div>
       </div>
         <p class="text-center"><a href="mailto:natgil.mundutegy@gmail.com">Nous contacter sur : natgil.mundutegy@gmail.com</a></p>
-        <form name="contact" method="POST" data-netlify="true" action="../pages/index.vue">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form name="ask-question"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="ask-question" />
+          <label>Your Name: <input type="text" name="name" /></label>
+          <label>Your Email: <input type="email" name="email" /></label>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+          <label>Message: <textarea name="message"></textarea></label>
+          <button type="submit">Send</button>
         </form>
         <p class="text-center"><a href="https://clementlecaloch.now.sh">Par Clément Le Caloch</a></p>
     </footer>
